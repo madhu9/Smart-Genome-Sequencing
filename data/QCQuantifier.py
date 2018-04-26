9382a067cbe53569614e3ae11ca92e7bc3985b6a
Asif Shikder <32821894+shikdernyc@users.noreply.github.com>
2018-04-26 14:14:49 -0400

    - psqs takes in optional parameter min_required_score - psqs completed. Returns the percentage of data that is above min_required_score - sld completed: returns the most frequent length and it's percentage - ac completed but might need more clarification - created basic blueprint for pbsc. need to figure out how we will calculate the tails of the data vs the middle

--- data/QCQuantifier.py
+++ data/QCQuantifier.py
@@ -1,4 +1,4 @@
-def pbsq(data, min_q_score = 30, col_index="Lower Quartile"):
+def pbsq(data, min_q_score=30, col_index="Lower Quartile"):
     """
     Score Per Base Sequence Quality from a Pandas Dataframe
     :param data: Pandas Dataframe containing PBSQ data
@@ -20,26 +20,25 @@ def pbsq(data, min_q_score = 30, col_index="Lower Quartile"):
             return score
         prev_data = prev_row.loc[[col_index]]
         prev_data = prev_data.values[0]
-        score += __score_pbsq(current_data, prev_data)
-        print(score)
+        score += __score_pbsq_slope(current_data, prev_data)
     return score
 
 
-def __score_pbsq(current_data, prev_data):
+def __score_pbsq_slope(current_data, prev_data):
     return 1 / abs((current_data - prev_data) / 2 + 1)
 
 
-def psqs(data, majoritiy_score):
+def psqs(data, min_required_score=30):
     """
-    Score Per Sequence Quality Score
+    Percent of Scores Per Sequence Quality above min score
     :param data: Pandas DataFrame of PSQS
-    :param majoritiy_score: What should majority of the scores be
-    :return: Score received by PSQS
+    :param min_required_score: What should majority of the scores be
+    :return: percentage over majority_score
     """
     # Majority of reads should have q score above 30
     # We can calculate percent area above 30 vs total area
     # Get the cutoff by statistical analysis of all data and determine optimal percent cutoff value
-    return None
+    return data[min_required_score:].sum() / data[:]['Count'].sum() * 100
 
 
 def pbsc(data):
@@ -73,19 +72,38 @@ def pbnc(data):
     # Area should be as close to 0 as possible
     # Closer to the front and back, higher the score
     # Spikes in the middle has lower score
-    return None
+    center_data_percentage = 70
+    center_data_range = (int((100 - 70) / 2), len(data) - int((100 - 70) / 2))
+
+    # ranges in beginning, middle and end range
+    # TODO: figure out how to calculate __pbnc_score_ends and __pbnc_score_middle
+    return __pbnc_score_ends(data[0:center_data_range[0]]) + \
+           __pbnc_score_middle(data[center_data_percentage[0]:center_data_range[1]]) + \
+           __pbnc_score_ends(data[center_data_range[1]:])
 
 
-def sld(data, percent_same_length):
+def __pbnc_score_ends(data_series):
+    # TODO: how the beginning and the end should be scored
+    return 0
+
+
+def __pbnc_score_middle(data_series):
+    # TODO:  how the middle of the series should be scored
+    return 0
+
+
+def sld(data):
     """
     Score SLD
     :param data: Pandas Dataframe of SLD
     :param percent_same_length: What percentage of length distribution should be the same length
-    :return: Score
+    :return: Score (Mode Length, Percentage of data of this length)
     """
     # Most point should be the same length
-    # Can be parsed from fastq file
-    return None
+    # Can be parsed from fastqc file
+    mode = data['Count'].max()
+    total = data['Count'].sum()
+    return mode, mode/total*100
 
 
 def ac(data):
@@ -94,6 +112,7 @@ def ac(data):
     :param data: AC Pandas DataFrame
     :return: 0 or 1 weather if fails or passes
     """
-    # Pass if everything is 0
-    # Fail if Exponential Line
-    return None
+    for column in data.columns:
+        if data[column].max() > 1:
+            return 0
+    return 1
