94bd74d66382c25bfd171f4225b818b514e7bbd6
Asif Shikder <32821894+shikdernyc@users.noreply.github.com>
2018-05-03 14:21:51 -0400

    Made it so QCSummary returns only a single number

--- data/QCManager.py
+++ data/QCManager.py
@@ -1,5 +1,7 @@
 from data.QCParser import FastQCParser
 from data import QCQuantifier as score
+import pandas as pd
+
 
 class FastQC:
     def __init__(self, file_loc):
@@ -8,10 +10,16 @@ class FastQC:
         self.__quantify()
 
     def __quantify(self):
-        self.summary["pbsq"] = score.pbsq(self.data.modules["pbsq"])
+        self.summary["pbsq"] = score.pbsq(self.data.modules["pbsq"])['score']
         self.summary["psqs"] = score.psqs(self.data.modules["psqs"])
-        self.summary["pbsc"] = score.pbsc(self.data.modules["pbsc"])
-        self.summary["psgc"] = score.psgc(self.data.modules["psgc"])
+        self.summary["pbsc"] = score.pbsc(self.data.modules["pbsc"])['avg_error']
+        # self.summary["psgc"] = score.psgc(self.data.modules["psgc"]) # TODO: Implement this later
         self.summary["pbnc"] = score.pbnc(self.data.modules["pbnc"])
         self.summary["sld"] = score.sld(self.data.modules["sld"])
         self.summary["ac"] = score.ac(self.data.modules["ac"])
+
+
+class FastQCDataPoint:
+    def __init__(self, qc_array):
+        self.qc_array = qc_array
+        self.summary = pd.DataFrame
