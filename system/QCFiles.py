726d8d99b05e0d5e462c32aa1cc7a0cde2a34a8a
Asif Shikder <32821894+shikdernyc@users.noreply.github.com>
2018-05-10 22:19:16 -0400

    - Created get_unique_qc(dir) which gets all unique sets of qc name - Created get_fastqc_pair_array which runs through an entire directory create arrays of fast_qc_pair files
    Refactored code to work as fastqc being pair files rather than single file

--- system/QCFiles.py
+++ system/QCFiles.py
@@ -1,19 +1,42 @@
 import os
 from data.QCManager import FastQC
+from data.QCManager import  FastQCPair
 from data.QCManager import FastQCDataPoint
 
 
-def get_fastqc_array(dir):
-    arr = []
+def get_unique_qc(dir):
+    unique_qc = set()
     for filename in os.listdir(dir):
         if filename.endswith(".txt"):
-            arr.append(FastQC(os.path.join(dir, filename)))
+            formatted_file_name = filename[:-18]
+            unique_qc.add(dir + "\\" + formatted_file_name)
+    return unique_qc
+
+
+def get_fastqc_pair_array(dir):
+    arr = dict()
+    unique_qc = get_unique_qc(dir)
+    for qc in unique_qc:
+        forward, reverse = get_fastqc_pair(qc)
+        arr[qc[qc.rindex('\\')+1:]] = FastQCPair(forward, reverse)
     return arr
 
 
-qc = get_fastqc_array("C:\\Users\\shikd\\Google Drive\\ByteSize\\20170712\\extracted")
+def get_qc_pairs_name(unique_qc_name):
+    return unique_qc_name + '_R1_001_fastqc.txt', unique_qc_name + '_R2_001_fastqc.txt'
 
-qc_list = FastQCDataPoint(qc)
 
-print(qc_list.report)
-qc_list.report.to_csv("extracted.csv")
+def get_fastqc_pair(unique_qc_name):
+    forward, reverse = get_qc_pairs_name(unique_qc_name)
+    forward_qc = FastQC(forward) if os.path.exists(forward) else None
+    reverse_qc = FastQC(reverse) if os.path.exists(forward) else None
+    return forward, reverse
+
+
+qc = get_fastqc_pair_array("C:\\Users\\shikd\\Google Drive\\ByteSize\\20170712\\extracted")
+
+
+# qc_list = FastQCDataPoint(qc)
+#
+# print(qc_list.report)
+# qc_list.report.to_csv("extracted.csv")
