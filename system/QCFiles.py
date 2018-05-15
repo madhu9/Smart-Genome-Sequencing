6031db31d4f3e9f644eb359e00009685710b315e
Asif Shikder <32821894+shikdernyc@users.noreply.github.com>
2018-05-15 11:29:43 -0400

    Created a get_fastqc_array function which takes a directory and returns all fastqc file stored in that directory
    Added comments to describe each function

--- system/QCFiles.py
+++ system/QCFiles.py
@@ -1,10 +1,43 @@
 import os
 from data.QCManager import FastQC
 from data.QCManager import  FastQCPair
-from data.QCManager import FastQCDataPoint
 
 
-def get_unique_qc(dir):
+def get_fastqc_pair_dict(dir):
+    """
+    Get matching forward and reverse filename location
+    :param dir: Location of all QC files
+    :return: (dict)Dictionary of each unique sequence filename and their (FastQCPair)paired file
+    """
+    qc_pair = dict()
+    unique_qc = __get_unique_qc(dir)
+    for qc in unique_qc:
+        forward, reverse = __get_fastqc_pair(qc)
+        qc_pair[qc[qc.rindex('\\')+1:]] = FastQCPair(forward, reverse)
+    return qc_pair
+
+
+def get_fastqc_array(dir):
+    """
+    Returns an array of QC files given it's directory
+    :param dir: Location of all QC files
+    :return: (Array) QC Files
+    """
+    qc_array = []
+    unique_qc = __get_unique_qc(dir)
+    for filename in os.listdir(dir):
+        if filename.endswith(".txt"):
+            file_loc = os.path.join(dir, filename)
+            qc_array.append(FastQC(file_loc))
+    return qc_array
+
+
+def __get_unique_qc(dir):
+    """
+    Given a directory, returns all the unique qc files (excluding the forward and the reverse prefix)
+    :param dir: Location of the directory
+    :return: (Set)All unique QC file location
+    """
     unique_qc = set()
     for filename in os.listdir(dir):
         if filename.endswith(".txt"):
@@ -13,30 +46,22 @@ def get_unique_qc(dir):
     return unique_qc
 
 
-def get_fastqc_pair_array(dir):
-    arr = dict()
-    unique_qc = get_unique_qc(dir)
-    for qc in unique_qc:
-        forward, reverse = get_fastqc_pair(qc)
-        arr[qc[qc.rindex('\\')+1:]] = FastQCPair(forward, reverse)
-    return arr
-
-
-def get_qc_pairs_name(unique_qc_name):
+def __get_qc_pairs_name(unique_qc_name):
+    """
+    Given an unique qc file, returns the forward and reverse filename
+    :param unique_qc_name: unique filename
+    :return: Tuple of (String)forward and (String)reverse file name
+    """
     return unique_qc_name + '_R1_001_fastqc.txt', unique_qc_name + '_R2_001_fastqc.txt'
 
 
-def get_fastqc_pair(unique_qc_name):
-    forward, reverse = get_qc_pairs_name(unique_qc_name)
+def __get_fastqc_pair(unique_qc_name):
+    """
+    Given an unique fastqc name, returns it's forward and reverse FastQC
+    :param unique_qc_name: Unique qc name
+    :return: (tuple)(FastQC)Forward, (FastQC)Reverse. Value of None indicates Path doesn't exists
+    """
+    forward, reverse = __get_qc_pairs_name(unique_qc_name)
     forward_qc = FastQC(forward) if os.path.exists(forward) else None
     reverse_qc = FastQC(reverse) if os.path.exists(forward) else None
-    return forward, reverse
-
-
-qc = get_fastqc_pair_array("C:\\Users\\shikd\\Google Drive\\ByteSize\\20170712\\extracted")
-
-
-# qc_list = FastQCDataPoint(qc)
-#
-# print(qc_list.report)
-# qc_list.report.to_csv("extracted.csv")
+    return forward_qc, reverse_qc
